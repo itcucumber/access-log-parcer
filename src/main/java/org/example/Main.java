f994ba775509760e8d85bd365d681f569b7683ea
Olga <xlaaaaam@gmail.com>
2024-10-17 14:43:48 +0300

    Работа со строками файла

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,57 @@
 package org.example;
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int fileCounter = 0;
-        Scanner scanner = new Scanner(System.in);
+        String path = "C:/Users/Surface Pro 9/Documents/access.log";
 
-        while (true) {
-            System.out.println("Введите путь к файлу:");
-            String path = scanner.nextLine();
-
+        try {
             File file = new File(path);
-            boolean fileExists = file.exists();
-            boolean isDirectory = file.isDirectory();
-
-            if (!fileExists || isDirectory) {
-                System.out.println("Файл не существует или указан путь к папке. Попробуйте еще раз.");
-                continue;
+            if (!file.exists()) {
+                throw new FileNotFoundException("Файл не найден: " + path);
+            }
+            if (!file.isFile()) {
+                throw new IOException("Указан путь к папке, не является путём к файлу: " + path);
             }
 
-            fileCounter++;
-            System.out.println("Путь указан верно. Это файл номер " + fileCounter);
+            int totalLines = 0;
+            int longestLineLength = 0;
+            int shortestLineLength = Integer.MAX_VALUE;
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    totalLines++;
+                    int length = line.length();
+
+                    if (length > 1024) {
+                        throw new LineTooLongException("В файле встретилась строка длиннее 1024 символов - " + length);
+                    }
+
+                    longestLineLength = Math.max(longestLineLength, length);
+                    shortestLineLength = Math.min(shortestLineLength, length);
+                }
+
+                System.out.println("Общее количество строк в файле: " + totalLines);
+                System.out.println("Длина самой длинной строки в файле: " + longestLineLength);
+                System.out.println("Длина самой короткой строки в файле: " + shortestLineLength);
+            }
+
+        } catch (LineTooLongException ex) {
+            System.err.println("Ошибка количества символов: " + ex.getMessage());
+        } catch (FileNotFoundException ex) {
+            System.err.println("Ошибка проверки существования файла: ");
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            System.err.println("Ошибка проверки пути: ");
+            ex.printStackTrace();
         }
+    }
+}
+
+class LineTooLongException extends RuntimeException {
+    public LineTooLongException(String message) {
+        super(message);
     }
 }
